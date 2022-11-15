549907427637da0686064d1a749506fdc1397bd2
bbookng <bbookng@gmail.com>
2022-11-16 08:13:18 +0900

    urltmp

--- back/accounts/urls.py
+++ back/accounts/urls.py
@@ -4,5 +4,11 @@ from django.urls import path
 app_name="accounts"
 
 urlpatterns = [
+    path("currentUser/", views.get_current_user_profile, name=""),
+    path('profile/<int:user_pk>/follow/', views.follow),
+    path("profile/<username>/", views.get_profile, name=""),
+    path('profile/<username>/movies/,', views.get_wishlists),
+    path('profile/<username>/reviews/', views.get_user_reviews),
+    path('profile/<username>/collections/', views.get_user_collections),
     
 ]
--- back/accounts/views.py
+++ back/accounts/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
 
 # Create your views here.
+# 현재 유저의 프로필 정보를 가져옴
+@api_view(['GET', 'PUT'])
+def get_current_user_profile(request):
+    pass
+
+# 특정 사용자의 프로필 정보를 가져옴
+@api_view(['GET'])
+def get_profile(request, username):
+    pass
+
+@api_view(['POST'])
+def follow(request, user_pk):
+    pass
+
+@api_view(['GET'])
+def get_wishlists(request, username):
+    pass
+
+@api_view(['GET'])
+def get_user_reviews(request, username):
+    pass
+
+@api_view(['GET'])
+def get_user_collections(request, username):
+    pass
