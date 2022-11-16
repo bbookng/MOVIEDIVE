0aaa010cd1c92ffbca82de8b9968986da488f01b
bbookng <bbookng@gmail.com>
2022-11-16 20:26:02 +0900

    signup, login

--- front/moviedive/src/router/index.js
+++ front/moviedive/src/router/index.js
@@ -1,16 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import SignUpView from '@/views/SignUpView'
+import LogInView from '@/views/LogInView'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/signup',
+    name: 'SignUpView',
+    component: SignUpView
   },
 
+  {
+    path: '/login',
+    name: 'LogInView',
+    component: LogInView
+  },
 ]
 
 const router = new VueRouter({
--- front/moviedive/src/store/index.js
+++ front/moviedive/src/store/index.js
@@ -5,12 +5,47 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    token: null,
   },
   getters: {
   },
   mutations: {
+      // 회원가입 && 로그인
+      SAVE_TOKEN(state, token) {
+        state.token = token
+        router.push({ name: 'ArticleView' })
+      }
   },
   actions: {
+    signUp(context, payload) {
+      axios({
+        method: 'post',
+        url: `${API_URL}/accounts/signup/`,
+        data: {
+          username: payload.username,
+          password1: payload.password1,
+          password2: payload.password2,
+        }
+      })
+        .then((res) => {
+          // console.log(res)
+          context.commit('SAVE_TOKEN', res.data.key)
+        })
+    },
+    logIn(context, payload) {
+      axios({
+        method: 'post',
+        url: `${API_URL}/accounts/login/`,
+        data: {
+          username: payload.username,
+          password: payload.password,
+        }
+      })
+        .then((res) => {
+          // console.log(res)
+          context.commit('SAVE_TOKEN', res.data.key)
+        })
+    },
   },
   modules: {
   }
