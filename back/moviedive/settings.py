ac4a1389c56a13461eafd97abffc0111d3549952
bbookng <bbookng@gmail.com>
2022-11-16 22:37:49 +0900

    Revert "mysqlclient"
    This reverts commit 52228632db1121a4de36c495eb445b187c5b5ace.

--- back/moviedive/settings.py
+++ back/moviedive/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from . import mysettings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = mysettings.SECRET_KEY
+SECRET_KEY = 'django-insecure-4-b=$)g@fb^fh+824e84v(heo!q*8(!rrl+^wb+i210!snvo9r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -137,7 +136,13 @@ WSGI_APPLICATION = 'moviedive.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = mysettings.DATABASES
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
