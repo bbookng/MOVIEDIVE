9b713f51d1e57b9a1fa1a4a485a4acde82a35ffc
bbookng <bbookng@gmail.com>
2022-11-28 17:21:49 +0900

    초기화

--- back/community/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Review, Comment
-
-
-class ThreadAdmin(admin.ModelAdmin):
-    list_display = ('movie',)
-
-admin.site.register(Review)
-admin.site.register(Comment)
--- back/data/data.md
+++
@@ -1,8 +0,0 @@
-```bash
-$ sqlite3 db.sqlite3
-> .mode csv
-.import --skip 1 data/movie.csv movies_movie
-.import --skip 1 data/genre.csv movies_genre
-.import --skip 1 data/movie_genre.csv movies_movie_genres
-```
-
